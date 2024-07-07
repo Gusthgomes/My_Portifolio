e2d7723c96b76ccbc3c9f47e77f7be38409171cc
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-07-07 14:52:47 -0300

    feat: adding new components to the grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,10 +1,11 @@
+import { gridItems } from "@/data";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
   return (
     <section id="about">
       <BentoGrid className="w-full py-20">
-        {[{ title: "Title1", description: "Desc1", id: 1 }].map((item) => (
+        {gridItems.map((item) => (
           <BentoGridItem
             id={item.id}
             key={item.id}
